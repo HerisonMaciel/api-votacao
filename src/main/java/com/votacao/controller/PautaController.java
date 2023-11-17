5bfadf2c900639bfc22685002e153bbc7d8b359e
HerisonMaciel <herisson2maciel@hotmail.com>
2023-11-16 21:53:13 -0300

    Criacao dos controladores

---
+++ src/main/java/com/votacao/controller/PautaController.java
@@ -0,0 +1,13 @@
+package com.votacao.controller;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/v1/pauta")
+public class PautaController {
+
+}
---
+++ src/main/java/com/votacao/controller/VotoController.java
@@ -0,0 +1,11 @@
+package com.votacao.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/v1/voto")
+public class VotoController {
+}
